ca122cdf3320a127b78004d01d646a650e0ade53
JamesIlling <JamesIlling@icloud.com>
2024-11-02 20:55:38 +0000

    Add project files.

---
+++ ConsoleApp1/GuardianCrossword.cs
@@ -0,0 +1,26 @@
+﻿namespace Crossword;
+
+internal class GuardianCrossword
+{
+    private readonly HttpClient _httpClient = new();
+
+    /// <summary>
+    ///     Attempt to download a cryptic crossword for a specific date.
+    /// </summary>
+    /// <param name="date">The date of the crossword to get</param>
+    /// <returns>True on success or false if an issue occurs.</returns>
+    public async Task<bool> DownloadAsync(DateTime date)
+    {
+        var url = new Uri($"https://crosswords-static.guim.co.uk/gdn.cryptic.{date:yyyyMMdd}.pdf");
+        var response = await _httpClient.GetAsync(url);
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine($"Got crossword for {date:yyyy-MM-dd}");
+            var filename = $"{date:yyyyMMdd}.pdf";
+            await File.WriteAllBytesAsync(filename, await response.Content.ReadAsByteArrayAsync());
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ ConsoleApp1/PdfMerger.cs
@@ -0,0 +1,41 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace Crossword;
+
+/// <summary>
+///     This class provides PDF Utility functions.
+/// </summary>
+internal static class PdfMerger
+{
+    /// <summary>
+    ///     Merge a selection of source PDF files into a single PDF.
+    /// </summary>
+    /// <param name="output">The path of the file to create</param>
+    /// <param name="sourceFiles">An array of the paths of the files to merge together.</param>
+    public static void Merge(string output, params string[] sourceFiles)
+    {
+        Console.WriteLine("Writing to single pdf");
+        using var outPdf = new PdfDocument();
+        foreach (var file in sourceFiles)
+        {
+            using var pdfFile = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+            CopyPages(pdfFile, outPdf);
+        }
+
+        outPdf.Save(output);
+    }
+
+    /// <summary>
+    ///     Copy an individual file from one document to another
+    /// </summary>
+    /// <param name="from">The source PDF document</param>
+    /// <param name="to">The destination PDF document</param>
+    private static void CopyPages(PdfDocument from, PdfDocument to)
+    {
+        for (var i = 0; i < from.PageCount; i++)
+        {
+            to.AddPage(from.Pages[i]);
+        }
+    }
+}
